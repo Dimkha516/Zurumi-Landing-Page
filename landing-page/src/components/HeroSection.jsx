5b91041a3ae2adafb2bf7e4dc552b49fd7648b41
bamba <dimkha516@gmail.com>
2025-10-04 18:37:41 +0000

    update home mobil app image

--- landing-page/src/components/HeroSection.jsx
+++ landing-page/src/components/HeroSection.jsx
@@ -67,7 +67,7 @@ const HeroSection = () => {
             <div className="relative">
               <div className="relative">
                 <img
-                  src="/Zurumi-Landing-Page/assets/affiche3.png"
+                  src="/Zurumi-Landing-Page/assets/affiche4.png"
                   alt="Aperçu application Zurumi"
                   // className="rounded-2xl w-64 sm:w-80 lg:w-88 xl:w-[50rem]"
                   // className="rounded-2xl w-72 sm:w-96 lg:w-[32rem] xl:w-[55rem]"
